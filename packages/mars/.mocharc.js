8a88bef901e83445318b5446c386b402350f2a5f
Marcin Wachulski <marcin@ethworks.io>
2021-11-04 19:31:17 +0100

    Enable e2e tests in yarn test/cover

--- packages/mars/.mocharc.js
+++ packages/mars/.mocharc.js
@@ -1,7 +1,10 @@
 process.env.NODE_ENV = 'test'
 module.exports = {
   extension: ['ts'],
-  spec: './test/**/*.test.ts',
+  spec: [
+    './test/**/*.test.ts',
+    './test/**/*.e2e.ts'
+  ],
   require: [
     'ts-node/register',
     'source-map-support/register'
