a6b83826ad96e7b171a42cee3c421ebbe150bfd2
marcin <marcin.wachulski@trusttoken.com>
2021-12-02 14:18:29 +0100

    Multisig deploy and multisig contract call
    Using CREATE2 deterministic addresses computation

--- packages/mars/src/gnosis/contractDeployer.ts
+++ packages/mars/src/gnosis/contractDeployer.ts
@@ -1,21 +1,47 @@
-import {Contract, providers, Signer} from "ethers";
-import {randomBytes} from "ethers/lib/utils";
+import {Contract, ethers, providers, Signer} from 'ethers'
+import {hexlify, randomBytes} from 'ethers/lib/utils'
 
 const createCall = require('./createCall.json')
 
+export interface DeterministicDeployment {
+  address: string,
+  transaction: providers.TransactionRequest
+}
+
 // TODO: rename to sth gnogis/create2? specific
 export class ContractDeployer {
-  constructor(private provider: providers.JsonRpcProvider) {
-  }
+  constructor(private provider: providers.JsonRpcProvider) {}
 
-  public async createDeploymentTx(unwrappedDeploymentTx: providers.TransactionRequest): Promise<providers.TransactionRequest> {
+  public async createDeploymentTx(
+    unwrappedDeploymentTx: providers.TransactionRequest,
+    contractBytecode: string
+  ): Promise<DeterministicDeployment> {
     const deployerContract = await this.getDeployerContract()
     const salt = randomBytes(32)
-    const deploymentTransaction = await deployerContract.populateTransaction.performCreate2(
-      0, unwrappedDeploymentTx.data, salt)
 
-    return deploymentTransaction
+    const transaction = await deployerContract.populateTransaction.performCreate2(
+      0,
+      unwrappedDeploymentTx.data,
+      salt
+    )
+    const address = this.computeCreate2Address(deployerContract.address, hexlify(salt), contractBytecode)
+
+    return {address, transaction}
   }
+
+  // from: https://gist.github.com/miguelmota/c9102d370a3c1891dbd23e821be82ae2
+  computeCreate2Address(creatorAddress: string, salt: string, byteCode: string): string {
+    const parts = [
+      'ff',
+      creatorAddress.slice(2),
+      salt.slice(2),
+      ethers.utils.keccak256(`0x${byteCode}`).slice(2),
+    ]
+
+    const partsHash = ethers.utils.keccak256(`0x${parts.join('')}`)
+    return `0x${partsHash.slice(-40)}`.toLowerCase()
+  }
+
 
   async getDeployerContract(): Promise<Contract> {
     const currentChainId = await this.provider.getNetwork()
--- packages/mars/test/e2e/testGnosis.e2e.ts
+++ packages/mars/test/e2e/testGnosis.e2e.ts
@@ -1,11 +1,13 @@
-import {ethers, providers, Signer} from 'ethers'
+import {Contract, ethers, providers, Signer} from 'ethers'
 import Safe, {EthersAdapter} from '@gnosis.pm/safe-core-sdk'
-import SafeServiceClient from "@gnosis.pm/safe-service-client";
-import {OperationType, SafeTransactionDataPartial} from '@gnosis.pm/safe-core-sdk-types'
-import {getDeployTx} from "../../src/execute/getDeployTx";
-import {SimpleContract} from "../fixtures/exampleArtifacts";
-import {AbiSymbol, Bytecode} from "../../src/symbols";
-import {ContractDeployer} from "../../src/gnosis/contractDeployer";
+import SafeServiceClient from '@gnosis.pm/safe-service-client'
+import {SafeTransaction, SafeTransactionDataPartial} from '@gnosis.pm/safe-core-sdk-types'
+import {getDeployTx} from '../../src/execute/getDeployTx'
+import {SimpleContract} from '../fixtures/exampleArtifacts'
+import {AbiSymbol, Bytecode} from '../../src/symbols'
+import {ContractDeployer} from '../../src/gnosis/contractDeployer'
+
+const SimpleContract__JSON = require('./../build/SimpleContract.json')
 
 // TODO: convenient way to pass env variables for mocha
 const config = {
@@ -14,19 +16,21 @@ const config = {
   infuraApiKey: '2d765c7dfe354b56bf2fc3cc03a8c34d', // marcin's created infura free tier subscription
   ethNetworkName: 'rinkeby', // in TT we test Gnosis Safes in Rinkeby
   ttSafe: '0x8772CD484C059EC5c61459a0abb5A45ece16701f', // TT Rinkeby Test Safe
-  owner: { // one of many owners of the TT Test Safe
+  owner: {
+    // one of many owners of the TT Test Safe
     address: '0x4a70cc993A25F0D57Fb37B8E8D5C7CcC0B24Cd7d',
-    privateKey: process.env.OWNER_PRIVATE_KEY as string
+    privateKey: process.env.OWNER_PRIVATE_KEY as string,
   },
-  delegate: { // one of the delegates
+  delegate: {
+    // one of the delegates
     address: '0x48A1B8fF5cEa06D95187f9A1B528D0c90554A179',
-    privateKey: process.env.DELEGATE_PRIVATE_KEY as string
-  }
+    privateKey: process.env.DELEGATE_PRIVATE_KEY as string,
+  },
 }
 
 // based on https://docs.gnosis-safe.io/build/sdks/core-sdk
-describe("Gnosis Safe as multisig contract deployment and interaction service in Rinkeby", () => {
-  let contractDeployer: ContractDeployer
+describe('Gnosis Safe as multisig contract deployment and interaction service in Rinkeby', () => {
+  let deployer: ContractDeployer
   let safeServiceClient: SafeServiceClient
   let owner: Signer
   let delegate: Signer
@@ -36,90 +40,75 @@ describe("Gnosis Safe as multisig contract deployment and interaction service in
   beforeEach(async () => {
     safeServiceClient = new SafeServiceClient(config.txServiceUri)
     const web3Provider = new providers.InfuraProvider(config.ethNetworkName, config.infuraApiKey)
-    contractDeployer = new ContractDeployer(web3Provider)
+    deployer = new ContractDeployer(web3Provider)
     owner = new ethers.Wallet(config.owner.privateKey, web3Provider)
     delegate = new ethers.Wallet(config.delegate.privateKey, web3Provider)
     safeByOwner = await Safe.create({
       ethAdapter: new EthersAdapter({ethers, signer: owner}),
-      safeAddress: config.ttSafe
+      safeAddress: config.ttSafe,
     })
     await safeByOwner.connect({})
     safeByDelegate = await Safe.create({
       ethAdapter: new EthersAdapter({ethers, signer: delegate}),
-      safeAddress: config.ttSafe
+      safeAddress: config.ttSafe,
     })
     await safeByDelegate.connect({})
   })
 
-  it("Prints Safe address and its owners", async () => {
+  it('Prints Safe address and its owners', async () => {
     const address = safeByOwner.getAddress()
     console.log(`Address: ${address}`)
 
     const owners = await safeByOwner.getOwners()
     console.log('Owners:')
-    owners.map(owner => console.log(owner))
+    owners.map((owner) => console.log(owner))
   })
 
   it('Adds a delegate if not already assigned', async () => {
     const delegateAddress = await delegate.getAddress()
     const delegates = (await safeServiceClient.getSafeDelegates(config.ttSafe))?.results ?? []
 
-    if (!delegates.some(d => d.delegate === delegateAddress)) {
+    if (!delegates.some((d) => d.delegate === delegateAddress)) {
       const addedDelegate = await safeServiceClient.addSafeDelegate({
         safe: config.ttSafe,
         delegate: delegateAddress,
         signer: owner,
-        label: 'marcin\'s delegate'
+        label: "marcin's delegate",
       })
 
       console.log(`Delegate ${addedDelegate} added.`)
     }
 
     console.log('Delegates:')
-    delegates.map(d => console.log(`${d.delegate} (${d.label}) added by ${d.delegator}`))
+    delegates.map((d) => console.log(`${d.delegate} (${d.label}) added by ${d.delegator}`))
   })
 
   // yup, this is a large test; we need a sequential ordering of steps which mocha does not support yet
-  // see: https://github.com/mochajs/mocha/issues/2684
-  it('Enqueues a contract deployment by a delegate, approves, executes and calls the created via multisig', async () => {
 
-    // Contract deployment
-    const simpleContractDeploymentTx = getDeployTx(SimpleContract[AbiSymbol], SimpleContract[Bytecode], [])
-    const safeSimpleContractDeploymentTx = await contractDeployer.createDeploymentTx(simpleContractDeploymentTx)
-    const safeScriptTx: SafeTransactionDataPartial = {
-      to: safeSimpleContractDeploymentTx.to,
-      data: safeSimpleContractDeploymentTx.data,
-      value: '0'
-    } as SafeTransactionDataPartial
-    const safeTransaction = await safeByDelegate.createTransaction(safeScriptTx)
-    const safeTransactionHash = await safeByDelegate.getTransactionHash(safeTransaction)
-    await safeServiceClient.proposeTransaction({
-      safeAddress: safeByDelegate.getAddress(),
-      safeTxHash: safeTransactionHash,
-      safeTransaction,
-      senderAddress: await delegate.getAddress(),
-    })
-    console.log(`Safe transaction proposed successfully: txHash = ${safeTransactionHash}`)
 
-    // Off-chain approve tx in Gnosis Transaction Service
-    // for an on-chain approval, you need to interact with the contract via the safe sdk (not the service client)
-    const confirmationSignature = await safeByOwner.signTransactionHash(safeTransactionHash);
-    const confirmationResponse = await safeServiceClient.confirmTransaction(safeTransactionHash, confirmationSignature.data)
-    console.log(`Confirmed with the owner ${safeByOwner.getAddress()}. Signature is: ${confirmationResponse.signature}`)
+// see: https://github.com/mochajs/mocha/issues/2684
+  it('Multisig-deploys a contract and multisig-calls a contract\'s operation', async () => {
+    // Contract deployment using multisig workflow
+    const bytecode = SimpleContract[Bytecode];
+    const directDeploymentTx = getDeployTx(SimpleContract[AbiSymbol], bytecode, [])
+    const {transaction: deploymentTx, address} = await deployer.createDeploymentTx(directDeploymentTx, bytecode)
 
-    // Execute transaction
+    const {
+      safeTransaction: safeDeploymentTx,
+      safeTransactionHash: safeDeploymentTxHash
+    } = await proposeInSafe(deploymentTx);
+    await confirmInSafe(safeDeploymentTxHash);
+    await executeInSafe(safeDeploymentTx);
 
-    // Get the address of a newly deployed contract
-
-    // Create contract interaction transaction with multisig
-
-    // Approve interaction
-
-    // Execute interaction
-
-    // Get the value of interaction
-
-    console.log(`Simple contract deployment via Safe tx hash: ${safeTransactionHash}`)
+    // Contract interaction using a separate multisig workflow
+    const simpleContract = new Contract(address, SimpleContract__JSON.abi, delegate)
+    const rawInteractionTx = await simpleContract.populateTransaction.hello()
+    const {
+      safeTransaction: safeInteractionTx,
+      safeTransactionHash: safeInteractionTxHash
+    } = await proposeInSafe(rawInteractionTx)
+    await confirmInSafe(safeInteractionTxHash)
+    await executeInSafe(safeInteractionTx)
   })
 
   describe('Utility pieces that can be called separately for diagnostic or debugging', () => {
@@ -132,6 +121,41 @@ describe("Gnosis Safe as multisig contract deployment and interaction service in
       console.log(JSON.stringify(tx, null, 2))
     })
   })
+
+  async function proposeInSafe(tx: providers.TransactionRequest) {
+    const safeScriptTx: SafeTransactionDataPartial = {
+      to: tx.to,
+      data: tx.data,
+      value: tx.value?.toString() ?? '0',
+    } as SafeTransactionDataPartial
+    const safeTransaction = await safeByDelegate.createTransaction(safeScriptTx)
+    const safeTransactionHash = await safeByDelegate.getTransactionHash(safeTransaction)
+    await safeServiceClient.proposeTransaction({
+      safeAddress: safeByDelegate.getAddress(),
+      safeTxHash: safeTransactionHash,
+      safeTransaction: safeTransaction,
+      senderAddress: await delegate.getAddress(),
+    })
+    console.log(`Safe transaction proposed successfully: txHash = ${safeTransactionHash}`)
+    return {safeTransaction, safeTransactionHash};
+  }
+
+  async function confirmInSafe(safeTransactionHash: string) {
+    // Off-chain approve tx in Gnosis Transaction Service
+    // for an on-chain approval, you need to interact with the contract via the safe sdk (not the service client)
+    const confirmationSignature = await safeByOwner.signTransactionHash(safeTransactionHash)
+    const confirmationResponse = await safeServiceClient.confirmTransaction(
+      safeTransactionHash,
+      confirmationSignature.data
+    )
+    console.log(`Confirmed with the owner ${safeByOwner.getAddress()}. Signature is: ${confirmationResponse.signature}`)
+  }
+
+  async function executeInSafe(safeDeploymentTx: SafeTransaction) {
+    // Execute transaction
+    const executionResult = await safeByOwner.executeTransaction(safeDeploymentTx)
+    console.log(`Execution tx hash: ${executionResult.hash}`)
+    await executionResult.transactionResponse?.wait()
+    console.log(`Deployment executed in the network!`)
+  }
 })
-
-
