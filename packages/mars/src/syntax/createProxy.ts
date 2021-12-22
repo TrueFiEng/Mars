2b42702de82ea99dec4aed1d685299e3b7f15b83
Ivan Rukhavets <ivanruch@gmail.com>
2021-12-22 13:31:20 +0100

    Update packages/mars/src/syntax/createProxy.ts

--- packages/mars/src/syntax/createProxy.ts
+++ packages/mars/src/syntax/createProxy.ts
@@ -22,6 +22,7 @@ function getImplementation(
     implementation(): Future<string>
   }>
 ): Future<string> {
+  // Storage slot defined in EIP-1967 https://eips.ethereum.org/EIPS/eip-1967
   const IMPLEMENTATION_SLOT = '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
   if (proxy.implementation) {
     return proxy.implementation()
