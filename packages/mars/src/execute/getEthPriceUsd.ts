288e5dc8275cce6a0764051650ed963180ed0442
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2022-06-08 17:50:06 +0200

    🎢 Upgrade to pnpm v7 (#114)

--- packages/mars/src/execute/getEthPriceUsd.ts
+++ packages/mars/src/execute/getEthPriceUsd.ts
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 export async function getEthPriceUsd(): Promise<number> {
   try {
     const res = await fetch('https://api.coinpaprika.com/v1/tickers/eth-ethereum')
-    const data = await res.json()
+    const data: any = await res.json()
     return data.quotes.USD.price
   } catch {
     return 0
